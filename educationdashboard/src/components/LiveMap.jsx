fa05391408dae4cb14ef7fffa2145dac95af80a2
steven <steven.spicher@gmail.com>
2023-07-28 10:17:16 -0600

    installed react-leaflet; loaded district layers; started legend;

---
+++ educationdashboard/src/components/LiveMap.jsx
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import {MapContainer, Marker, Popup, TileLayer, GeoJSON, } from "react-leaflet";
+import districtData from "../mapping/district_demographic_join.json"
+import etlData from "../mapping/etl_district_full_data_with_geometries.json"
+import Legend from "../components/Legend"
+const LiveMap = () => {
+    // const [map, setMap] = useState(null);
+
+    return (
+    <MapContainer id={"map"} center={[33.4, -81.1]} zoom={7} scrollWheelZoom={true} >
+        <TileLayer
+            attribution=' Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Legend/>
+        <GeoJSON data={etlData} />
+        {/*<GeoJSON data={districtData} />*/}
+        {/*<Marker position={[33.4, -81.1]}>*/}
+        {/*    <Popup>*/}
+        {/*        A pretty CSS3 popup. <br /> Easily customizable.*/}
+        {/*    </Popup>*/}
+        {/*</Marker>*/}
+    </MapContainer>
+        )
+};
+
+export default LiveMap;
