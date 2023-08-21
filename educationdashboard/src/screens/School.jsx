492e9f9387c3a098dd197427620c099645ff5386
steven <steven.spicher@gmail.com>
2023-08-21 09:52:15 -0600

    styling

--- educationdashboard/src/screens/School.jsx
+++ educationdashboard/src/screens/School.jsx
@@ -1,6 +1,4 @@
-import "../css/search.css";
 import "../css/schools.css";
-import "../css/district.css";
 import home from "../assets/home.png"
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {Chart} from "react-google-charts";
@@ -22,162 +20,168 @@ import {
     other
 } from "../data/data"
 
-
-// const options = {
-//     chart: {
-//         // title: "Students with Positive Reading Score",
-//         // subtitle: "Sales, Expenses, and Profit: 2014-2017",
-//     },
-// };
 const School = () => {
     return (
         <>
 
-            {/*<div className="container">*/}
-            <Container sx={{maxWidth: "800"}}>
+            <Container fluid>
                 <div className="search-block">
                     <div className="app-search">
                         <a href="../../"><img src={home}/></a>
-                        <input id="fill-box" type="text" placeholder="Search school or district ..."/>
-                        <i className="fa fa-search"></i>
+                        <div className="search-row">
+                            <input id="fill-box" type="text" placeholder="Search school or district ..."/>
+                            <i className="fa fa-search"></i>
+
+                        </div>
                     </div>
-                </div>
-                <div id="suggestions-list" className="list-items"></div>
-                <div className="search-row">
+                    <div id="suggestions-list" className="list-items"></div>
                 </div>
                 {/*</div>*/}
-                <div id="title">
-                    <div className={"h1"}>Iva Elementary School</div>
-                    <div className={"h2"}>Anderson School District 3</div>
-                    <div className={"h4"}>803 Antreville Hwy, Iva</div>
-                    <div className={"h4"}>385 students, 30 teachers</div>
-                </div>
+                <div className="container">
 
-                <div className={"chart-container graphic-style"}>
-                    <Stack>
-
-
-                    <h5>Academic Performance</h5>
-
-                    <Stack className="chart-container" direction={"row"}>
-                        <Chart
-                            chartType="ColumnChart"
-                            width="200px"
-                            height="50%"
-                            data={readingScoreData}
-                            options={options}
-                        />
-                        <Chart
-                            chartType="ColumnChart"
-                            width="200px"
-                            height="50%"
-                            data={mathScoreData}
-                            options={options}
-                        />
-                        <Chart
-                            chartType="ColumnChart"
-                            width="200px"
-                            height="50%"
-                            data={scienceScoreData}
-                            options={options}
-                        />
-                    </Stack>
-                        <Stack className="chart-container" direction={"row"}>
-                            <Chart
-                                chartType="ColumnChart"
-                                width="200px"
-                                height="50%"
-                                data={onTimeGraduationRate}
-                                options={options}
-                            />
-                            <Chart
-                                chartType="ColumnChart"
-                                width="200px"
-                                height="50%"
-                                data={dropoutPercentage}
-                                options={options}
-                            />
-                        </Stack>
-                        <Stack className="chart-container" direction={"row"}>
-                            <Chart
-                                chartType="ColumnChart"
-                                width="200px"
-                                height="50%"
-                                data={collegeReadyDiplomaEarners}
-                                options={options}
-                            />
-                            <Chart
-                                chartType="ColumnChart"
-                                width="200px"
-                                height="50%"
-                                data={careerReadyDiplomaEarners}
-                                options={options}
-                            />
-                        </Stack>
-
-                    </Stack>
-                </div>
-                <div className={"chart-container"}>
+                    <div id="title">
+                        <div className={"h1"}>Iva Elementary School</div>
+                        <div className={"h3"}>Anderson School District 3</div>
+                        <div className={"h4"}>803 Antreville Hwy, Iva</div>
+                        <div className={"h4"}>385 students, 30 teachers</div>
+                    </div>
 
                     <div className="col-12 col-md-12 graphic">
-                        <div className="graphic-style">
+                        <div className={"chart-graphic-style"}>
                             <Stack>
-                            <h5>Demographics</h5>
-                            <Stack className="chart-container" direction={"row"}>
-                                <Chart
-                                    chartType="ColumnChart"
-                                    width="200px"
-                                    height="50%"
-                                    data={studentsInPoverty}
-                                    options={options}
-                                />
-                                <Chart
-                                    chartType="ColumnChart"
-                                    width="200px"
-                                    height="50%"
-                                    data={studentsWithDisabilities}
-                                    options={options}
-                                />
-                                <Chart
-                                    chartType="ColumnChart"
-                                    width="200px"
-                                    height="50%"
-                                    data={englishLanguageLearningStudents}
-                                    options={options}
-                                />
-                            </Stack>
-                                <Stack className="chart-container" direction={"row"}>
+                                <h5 className={"chart-title"}>Academic Performance</h5>
+                                <Stack
+                                    className="chart-container"
+                                    direction="row"
+                                    justifyContent="space-evenly"
+                                    alignItems="center"
+                                    // spacing={4}
+                                >
                                     <Chart
                                         chartType="ColumnChart"
                                         width="200px"
                                         height="50%"
-                                        data={white}
+                                        data={readingScoreData}
                                         options={options}
                                     />
                                     <Chart
                                         chartType="ColumnChart"
                                         width="200px"
                                         height="50%"
-                                        data={black}
+                                        data={mathScoreData}
                                         options={options}
                                     />
                                     <Chart
                                         chartType="ColumnChart"
                                         width="200px"
                                         height="50%"
-                                        data={other}
+                                        data={scienceScoreData}
                                         options={options}
                                     />
                                 </Stack>
+                                {/*<Stack className="chart-container" direction={"row"}>*/}
+                                {/*    <Chart*/}
+                                {/*        chartType="ColumnChart"*/}
+                                {/*        width="200px"*/}
+                                {/*        height="50%"*/}
+                                {/*        data={onTimeGraduationRate}*/}
+                                {/*        options={options}*/}
+                                {/*    />*/}
+                                {/*    <Chart*/}
+                                {/*        chartType="ColumnChart"*/}
+                                {/*        width="200px"*/}
+                                {/*        height="50%"*/}
+                                {/*        data={dropoutPercentage}*/}
+                                {/*        options={options}*/}
+                                {/*    />*/}
+                                {/*</Stack>*/}
+                                {/*<Stack className="chart-container" direction={"row"}>*/}
+                                {/*    <Chart*/}
+                                {/*        chartType="ColumnChart"*/}
+                                {/*        width="200px"*/}
+                                {/*        height="50%"*/}
+                                {/*        data={collegeReadyDiplomaEarners}*/}
+                                {/*        options={options}*/}
+                                {/*    />*/}
+                                {/*    <Chart*/}
+                                {/*        chartType="ColumnChart"*/}
+                                {/*        width="200px"*/}
+                                {/*        height="50%"*/}
+                                {/*        data={careerReadyDiplomaEarners}*/}
+                                {/*        options={options}*/}
+                                {/*    />*/}
+                                {/*</Stack>*/}
 
-
-                        </Stack>
+                            </Stack>
                         </div>
                     </div>
-                </div>
-                <div className={"card-container"}>
-                    <div className="col-md-4 graphic">
-                        <Card className={"graphic-style card-width"}>
+                    <div className="col-12 col-md-4 graphic">
+                            <div className="chart-graphic-style">
+                                <Stack
+
+                                >
+                                    <h5 className={"chart-title"}>Demographics</h5>
+
+                                    <Stack
+                                        className="chart-container"
+                                        direction="row"
+                                        justifyContent="space-evenly"
+                                        alignItems="center"
+                                    >
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={studentsInPoverty}
+                                            options={options}
+                                        />
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={studentsWithDisabilities}
+                                            options={options}
+                                        />
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={englishLanguageLearningStudents}
+                                            options={options}
+                                        />
+                                    </Stack>
+                                    <Stack className="chart-container"    direction="row"
+                                           justifyContent="space-evenly"
+                                           alignItems="center">
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={white}
+                                            options={options}
+                                        />
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={black}
+                                            options={options}
+                                        />
+                                        <Chart
+                                            chartType="ColumnChart"
+                                            width="200px"
+                                            height="50%"
+                                            data={other}
+                                            options={options}
+                                        />
+                                    </Stack>
+
+
+                                </Stack>
+                        </div>
+                    </div>
+                    <div className={"card-container col-12 col-md-12  graphic"}>
+                        <Card className={"card-graphic-style card-width"}>
                             <Card.Body>
                                 <Card.Title className="card-total">0</Card.Title>
                                 <Card.Text>
@@ -185,9 +189,9 @@ const School = () => {
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </div>
-                    <div className="col-md-4 graphic">
-                        <Card className="graphic-style card-width">
+                        {/*</div>*/}
+                        {/*<div className="col-md-4 graphic">*/}
+                        <Card className="card-graphic-style card-width">
                             <Card.Body>
                                 <Card.Title className="card-total">$55,206</Card.Title>
                                 <Card.Text>
@@ -195,9 +199,9 @@ const School = () => {
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </div>
-                    <div className="col-md-4 graphic">
-                        <Card className={"graphic-style card-width"}>
+                        {/*</div>*/}
+                        {/*<div className="col-md-4 graphic">*/}
+                        <Card className={"card-graphic-style card-width"}>
                             <Card.Body>
                                 <Card.Title className="card-total">95%</Card.Title>
                                 <Card.Text>
@@ -205,9 +209,9 @@ const School = () => {
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </div>
-                    <div className="col-12 col-md-4 graphic">
-                        <Card className="graphic-style card-width">
+                        {/*</div>*/}
+                        {/*<div className="col-12 col-md-4 graphic">*/}
+                        <Card className="card-graphic-style card-width">
                             <Card.Body>
                                 <Card.Title className="card-total">100%</Card.Title>
                                 <Card.Text>
@@ -215,19 +219,19 @@ const School = () => {
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </div>
-                    <div className="col-12 col-md-4 graphic">
-                        <Card className="graphic-style card-width">
+                        {/*</div>*/}
+                        {/*// <div className="col-12 col-md-4 graphic">*/}
+                        <Card className="card-graphic-style card-width ">
                             <Card.Body>
                                 <Card.Title className="card-total">8</Card.Title>
-                                <Card.Text>
+                                <Card.Text className={"h5"}>
                                     Number of violent assaults
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-                    </div>
+                        </div>
+                    {/*</div>*/}
                 </div>
-                {/*</div>*/}
 
 
             </Container>
